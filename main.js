849aafaab1e996504c5acb98922ee67c96adcd7b
Samyak Bhoyar <78425159+safdhuehyuyh@users.noreply.github.com>
2021-04-30 12:50:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Pljt69ahJ/model.json',modelLoaded)
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5Xq0cxSiF/model.json',modelLoaded)
 }
 function draw(){
     image(video, 0, 0, 300, 300);
